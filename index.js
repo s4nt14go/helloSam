a9b0551974c6f868d61e8824994455511fd612e1
s4nt14go <santiagopergamo@gmail.com>
2020-08-02 17:34:54 -0300

    chore

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var time = require('time');
 exports.handler = (event, context, callback) => {
   console.log('process.env', process.env);
-  console.log('3');
+  console.log('4');
   var currentTime = new time.Date();
   currentTime.setTimezone("America/Argentina/Cordoba");
   callback(null, {
