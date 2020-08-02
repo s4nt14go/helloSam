9249167c8ed833b468b6453493fb81dae089ceec
santiago <santiago.pergamo@dinocloudconsulting.com>
2020-08-02 16:48:21 -0300

    Timezone fixed

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var time = require('time');
 exports.handler = (event, context, callback) => {
   console.log('process.env', process.env);
   var currentTime = new time.Date();
-  currentTime.setTimezone("America/Argentina");
+  currentTime.setTimezone("America/Argentina/Cordoba");
   callback(null, {
     statusCode: '200',
     body: 'The time in Los Angeles is: ' + currentTime.toString(),
