ac5ed7d50df3072a294dc582ab05a6e78a48764f
santiago <santiago.pergamo@dinocloudconsulting.com>
2020-08-02 16:41:35 -0300

    .gitignore added, timezone changed

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var time = require('time');
 exports.handler = (event, context, callback) => {
   console.log('process.env', process.env);
   var currentTime = new time.Date();
-  currentTime.setTimezone("America/Los_Angeles");
+  currentTime.setTimezone("America/Argentina");
   callback(null, {
     statusCode: '200',
     body: 'The time in Los Angeles is: ' + currentTime.toString(),
