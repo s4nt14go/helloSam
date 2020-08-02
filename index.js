152b0c5deee34d5ebb040306931e4767e3b9c411
santiago <santiago.pergamo@dinocloudconsulting.com>
2020-08-02 12:04:07 -0300

    First commit

---
+++ index.js
@@ -0,0 +1,10 @@
+var time = require('time');
+exports.handler = (event, context, callback) => {
+  console.log('process.env', process.env);
+  var currentTime = new time.Date();
+  currentTime.setTimezone("America/Los_Angeles");
+  callback(null, {
+    statusCode: '200',
+    body: 'The time in Los Angeles is: ' + currentTime.toString(),
+  });
+};
